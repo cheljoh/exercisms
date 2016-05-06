43a019610572480febd4aaee1027b1a934cce965
Chelsea Johnson <cheljoh@gmail.com>
2016-05-05 20:30:45 -0500

    better version of rna-transcription

--- javascript/rna-transcription/rna-transcription.js
+++ javascript/rna-transcription/rna-transcription.js
@@ -2,20 +2,15 @@ var DnaTranscriber = function() {};
 
 DnaTranscriber.prototype.toRna = function(dna) {
   var strand = ''
+  var complements = {
+    'G':'C',
+    'C':'G',
+    'T':'A',
+    'A':'U'
+  }
   for (i=0; i < dna.length; i++){
-    if (dna[i] == 'C'){
-      strand += 'G'
-    }
-    else if (dna[i] == 'G'){
-      strand += 'C'
-    }
-    else if (dna[i] == 'A'){
-      strand += 'U'
-    }
-    else if (dna[i] == 'T'){
-      strand += 'A'
-    }
-  };
+    strand += complements[dna[i]]
+  }
   return strand
 };
 
