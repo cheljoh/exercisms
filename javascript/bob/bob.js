42137e19f2dd44e8eece0961ca3a9066677f4db9
Chelsea Johnson <cheljoh@gmail.com>
2016-05-15 16:30:00 -0600

    made more methods for conditional paths

--- javascript/bob/bob.js
+++ javascript/bob/bob.js
@@ -1,13 +1,13 @@
 var Bob = function() {};
 
 Bob.prototype.hey = function(input) {
-  if(checkIfUpcase(input) && (/[a-z]/i.test(input))){
+  if(checkIfUpcase(input) && containsLetters(input)){
     return 'Whoa, chill out!'
   }
-  else if(input[input.length-1] === '?'){
+  else if(checkQuestion(input)){
     return 'Sure.'
   }
-  else if(!(/\S/.test(input))){
+  else if(inputIsBlankOrWhitespace(input)){
     return 'Fine. Be that way!'
   }
   return 'Whatever.'
@@ -15,6 +15,18 @@ Bob.prototype.hey = function(input) {
 
 function checkIfUpcase(input){
   return input === input.toUpperCase()
+}
+
+function containsLetters(input){
+  return /[a-z]/i.test(input)
+}
+
+function checkQuestion(input){
+  return input[input.length-1] === '?'
+}
+
+function inputIsBlankOrWhitespace(input){
+  return !(/\S/.test(input))
 }
 
 module.exports = Bob;
