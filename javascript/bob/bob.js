d68468cffadd7e58dda3087e480484234bae010a
Chelsea Johnson <cheljoh@gmail.com>
2016-05-15 15:30:58 -0600

    finished bob

--- javascript/bob/bob.js
+++ javascript/bob/bob.js
@@ -4,8 +4,11 @@ Bob.prototype.hey = function(input) {
   if(checkIfUpcase(input) && (/[a-z]/i.test(input))){
     return 'Whoa, chill out!'
   }
-  else if(input.includes('?')){
+  else if(input[input.length-1] === '?'){
     return 'Sure.'
+  }
+  else if(!(/\S/.test(input))){
+    return 'Fine. Be that way!'
   }
   else {
     return "Whatever."
